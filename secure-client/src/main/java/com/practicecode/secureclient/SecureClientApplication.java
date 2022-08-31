86ca76fe0adedd827259b583b5073120b267faeb
ravindugm <ravindumiyuranga@gmail.com>
2022-09-01 00:55:25 +0530

    Initial commit

---
+++ secure-client/src/main/java/com/practicecode/secureclient/SecureClientApplication.java
@@ -0,0 +1,13 @@
+package com.practicecode.secureclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureClientApplication.class, args);
+	}
+
+}
---
+++ secure-client/src/test/java/com/practicecode/secureclient/SecureClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practicecode.secureclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
