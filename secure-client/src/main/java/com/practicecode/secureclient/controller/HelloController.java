1abb27c567464f27c96ca8d47b808b3e37178a33
ravindugm <ravindumiyuranga@gmail.com>
2022-09-24 13:40:59 +0530

    Add config server

--- secure-client/src/main/java/com/practicecode/secureclient/controller/HelloController.java
+++ secure-client/src/main/java/com/practicecode/secureclient/controller/HelloController.java
@@ -1,16 +1,17 @@
 package com.practicecode.secureclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${message: Hello User}")
     private String message;
 
     @GetMapping("/hello")
     public String hello() {
-//        return "Hello, your code us working";
         return message;
     }
 }
