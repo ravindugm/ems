dda930bfdc6cb6e5ef6db20537cad952b6fbd7a0
ravindugm <ravindumiyuranga@gmail.com>
2022-09-25 22:35:00 +0530

    Connect config server with secure client

--- secure-client/src/main/java/com/practicecode/secureclient/controller/HelloController.java
+++ secure-client/src/main/java/com/practicecode/secureclient/controller/HelloController.java
@@ -1,13 +1,15 @@
 package com.practicecode.secureclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class HelloController {
 
-    @Value("${message: Hello User}")
+    @Value("${default.test.message: Hello User}")
     private String message;
 
     @GetMapping("/hello")
